5b1fbe21ba1460fe6d7283e6105ef69cddaf4602
Daniel.Wellermann <daniel.wellermann@plansite.de>
2023-10-05 21:25:54 +0200

    Setup

---
+++ src/app.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <div class="container py-4 px-3 mx-auto">
+            <h1>Hello</h1>
+            <p>Welcome to vue3-sfcAdvanTable</p>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './scss/style.scss'
+
+import * as bootstrap from 'bootstrap'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+createApp(App).mount('#app')
